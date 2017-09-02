26af7e7c4f374f76833e2e387b68871b9a81afc5
Amine Ghilazghi <amineteclai@gmail.com>
2017-09-02 19:59:48 -0400

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create(),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+minifyCss = require('gulp-minify-css'),
+autoprefixer = require('gulp-autoprefixer'),
+plumber = require('gulp-plumber'),
+sourcemaps = require('gulp-sourcemaps');
+
+// File paths
+var CSS_DIST_PATH = './dist/css';
+var SCRIPT_DIST_PATH = './dist/js';
+var SCRIPTS_PATH = './js/*.js';
+var CSS_PATH = './css/*.css';
+
+
+gulp.task('styles', function() {
+    return gulp.src(['./css/reset.css', CSS_PATH])
+        .pipe(plumber(function (err) {
+            console.log('Styles Task Error');
+            console.log(err);
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(concat('styles.css'))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(CSS_DIST_PATH));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(SCRIPTS_PATH)
+        .pipe(uglify())
+        .pipe(gulp.dest(SCRIPT_DIST_PATH));
+});
+
+gulp.task('watch', function() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch('./index.html', function(){
+        browserSync.reload();
+    });
+
+    watch(CSS_PATH, function() {
+        gulp.start('styles');
+        browserSync.reload();
+    });
+
+    watch(SCRIPTS_PATH, function() {
+        gulp.start('scripts')
+        browserSync.reload();
+    })
+});
