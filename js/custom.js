70ba1633364814bdf6ed338f3335ebfaf3669d54
Amine Ghilazghi <amineteclai@gmail.com>
2017-09-14 17:26:01 -0400

    Completed adding sticky navigation on scroll

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
-    
+    /* sticky nav */
+    $('.js-section-features').waypoint(function(direction){
+        if(direction === "down"){
+            $('nav').addClass('sticky');
+        }else {
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset: '85%'
+    });
    
 });
